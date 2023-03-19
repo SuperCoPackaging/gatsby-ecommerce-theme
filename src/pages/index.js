ac47a6b02184cbc94278ac5b87a56afa56187414
SuperCoPackaging <128325131+SuperCoPackaging@users.noreply.github.com>
2023-03-19 22:02:04 +0000

    Subtitle text

--- src/pages/index.js
+++ src/pages/index.js
@@ -31,7 +31,7 @@ const IndexPage = () => {
         maxWidth={'500px'}
         image={'/banner1.png'}
         title={'SuperCo Packaging'}
-        subtitle={'Discover Autumn Winter 2021'}
+        subtitle={'All Products / 2023'}
         ctaText={'shop now'}
         ctaAction={goToShop}
       />
